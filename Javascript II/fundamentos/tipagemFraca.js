5d3aad46f27e3b93fd2fe1d758818fda73301f5a
Matheuscoe07 <100366370+Matheuscoe07@users.noreply.github.com>
2023-07-30 20:43:22 -0300

    Tipagem fraca

---
+++ Javascript II/fundamentos/tipagemFraca.js
@@ -0,0 +1,16 @@
+let qualquer = 'Legal'
+console.log(qualquer)
+console.log(typeof qualquer) //mostra o tipo da variavel
+
+qualquer = 3.1415
+console.log(qualquer)
+console.log(typeof qualquer)
+
+/*
+o javascript é conhecido como tipagem fraca, ou seja,
+nesse linguagem de programação os tipos das variáveis
+também podem ser alterados de acordo com o elemento que
+se esta trabalhando
+*/
+
+//Evitar nomes genéricos e siglas!
