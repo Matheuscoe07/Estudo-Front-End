7bde8c74b6a6912c9da24f27be2bb874e5148d89
Matheuscoe07 <100366370+Matheuscoe07@users.noreply.github.com>
2023-07-30 20:09:03 -0300

    Variaveis e Constantes

---
+++ Javascript II/fundamentos/variaveisEConstantes.js
@@ -0,0 +1,25 @@
+//palavra reservada identificador simbolo de atribuicao valor
+
+var a = 3 
+
+//identificadores são de extrema importânica
+
+let b = 4
+
+//let é mais moderno que var e é recomendado de usar mais corriqueiramente
+
+var a = 30
+b = 40
+
+console.log(a, b)
+
+//por ser variável o valor pode ser alterado quando necessário
+
+a = 300
+b = 400
+
+console.log(a, b)
+
+const c = 10
+
+//valores const são constantes, portanto não se alteram
