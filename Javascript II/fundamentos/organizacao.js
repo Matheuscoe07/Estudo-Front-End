fc7c22c934f8e4e32347db0ea8b815f499a91b04
Matheuscoe07 <100366370+Matheuscoe07@users.noreply.github.com>
2023-07-30 19:56:56 -0300

    Organizacao da linguagem

---
+++ Javascript II/fundamentos/organizacao.js
@@ -0,0 +1,11 @@
+console.log("Hello World") //senteça de código
+
+{ 
+    //Bloco de códigos
+}
+
+/*
+EXECUTANDO JAVASCRIPT
+- sites próprios para execução como replit e js fiddle
+- console (dentro do browser e próprio do sistema operacional)
+*/
