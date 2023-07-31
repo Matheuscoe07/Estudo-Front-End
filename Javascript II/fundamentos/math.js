15b71ab794cb0af133dccb1f6ae592d37e1966ac
Matheuscoe07 <100366370+Matheuscoe07@users.noreply.github.com>
2023-07-31 08:37:52 -0300

    Objeto Math

---
+++ Javascript II/fundamentos/math.js
@@ -0,0 +1,9 @@
+//Math é um objeto do JS que permite realizar algumas operações
+
+const raio = 5.6
+
+const area = Math.PI  * Math.pow(raio, 2)
+//           Valor pi            raio elevado ao quadrado
+
+console.log(area)
+console.log(typeof Math) //Definido math como um object
