546272fbdf7b601f581c33b5c0bf761dfc2b4b11
Matheuscoe07 <100366370+Matheuscoe07@users.noreply.github.com>
2023-07-31 09:30:20 -0300

    Tipo boolean

---
+++ Javascript II/fundamentos/boolean.js
@@ -0,0 +1,25 @@
+let isAtivo = false
+console.log(isAtivo)
+
+isAtivo = true
+console.log(isAtivo)
+
+isAtivo = 1
+console.log(!!isAtivo) //Deve converter para booleano, pois se não considera numerico
+
+console.log('Os verdadeiros') //Os verdadeiros
+console.log(!!3)
+console.log(!!-1)
+console.log(!!' ')
+console.log(!![])
+console.log(!!{})
+console.log(!!Infinity)
+console.log(!!(isAtivo = true))
+
+console.log('Os falsos') //Os falsos
+console.log(!!0)
+console.log(!!'')
+console.log(!!null)
+console.log(!!NaN)
+console.log(!!undefined)
+console.log(!!(isAtivo = false))
