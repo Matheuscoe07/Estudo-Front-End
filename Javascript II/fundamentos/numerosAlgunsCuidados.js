ca2e33062e48a072c9fc8177ecfd373923dff945
Matheuscoe07 <100366370+Matheuscoe07@users.noreply.github.com>
2023-07-31 08:34:47 -0300

    Alguns cuidados com o tipo numérico

---
+++ Javascript II/fundamentos/numerosAlgunsCuidados.js
@@ -0,0 +1,11 @@
+console.log(7 / 0) //Devolve o valor infinity
+
+console.log("10" / 2) //Le o valor interno da string e ve se consegue transformar em valor numerico
+
+console.log("Show!" * 2)
+
+console.log(0.1 + 0.7) //Devolve uma imprecisão no ponto flutuante
+
+//console.log(10.toString()) //simplesmente não funciona
+
+console.log((10.375).toFixed(2)) //Trava em duas casas decimais e arredonda adequadamente
