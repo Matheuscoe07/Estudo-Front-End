73c7c6f29e4884e4183eab9410445c5f31e0e919
Matheuscoe07 <100366370+Matheuscoe07@users.noreply.github.com>
2023-08-01 11:04:14 -0300

    let

---
+++ Javascript II/fundamentos/let.js
@@ -0,0 +1,8 @@
+let numero = 1
+{
+    let numero = 2
+    console.log('dentro =', numero)
+}
+console.log('fora =', numero)
+
+//A unica diferença é que esta disponivel globalmente quando nao esta restrita em bloco
